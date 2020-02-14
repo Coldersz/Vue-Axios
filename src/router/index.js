0cd80a816f3138120ba3f46028c5f939279603b9
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-14 11:26:30 +0700

    Added routes for posts, single post, and albums

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/posts',
+    name: 'Posts',
+    component: () => import('../views/Post/Posts.vue')
+  },
+  {
+    path: '/posts/:id',
+    name: 'Posts',
+    component: () => import('../views/Post/SinglePost.vue')
+  },
+  {
+    path: '/albums',
+    name: 'Albums',
+    component: () => import('../views/Album.vue')
   }
 ]
 
